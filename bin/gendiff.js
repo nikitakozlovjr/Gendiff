ecb3f1fdbfa440b1035132d0566976352e5fe7f5
nikitakozlovjr <nikita3726898@gmail.com>
2023-11-20 23:11:13 +0300

    Добавили опцию выбора формата и получение аргументов

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -6,6 +6,8 @@ import { program } from 'commander';
 program
     .name('genDiff')
     .description('Compares two configuration files and shows a difference.')
-    .version('1.0.0');
+    .version('1.0.0')
+    .option('-f, --format <type>', 'output format')
+    .arguments('<filepath1> <filepath2>');
 
 program.parse();
