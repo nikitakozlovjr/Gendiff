20ee36b1b9e2dce3338c8268c92a6d8cf2ecb4a4
nikitakozlovjr <nikita3726898@gmail.com>
2023-11-20 23:10:14 +0300

    Добавили исполняемый файл gendiff и настроили его -h (help)

---
+++ bin/gendiff.js
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+
+import { program } from 'commander';
+
+
+program
+    .name('genDiff')
+    .description('Compares two configuration files and shows a difference.')
+    .version('1.0.0');
+
+program.parse();
