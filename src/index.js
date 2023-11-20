f4755f72737f5dc0a4575672bc055f291b58d505
nikitakozlovjr <nikita3726898@gmail.com>
2023-11-21 00:04:56 +0300

    Написали получение изменений в объекте

--- src/index.js
+++ src/index.js
@@ -7,7 +7,27 @@ const genDiff = (file1, file2) => {
     const filepath1 = buldpath(file1);
     const filepath2 = buldpath(file2);
 
-    return [filepath1, filepath2].map((filepath) => parse(filepath));
+    const [data1, data2] = [filepath1, filepath2].map((filepath) => parse(filepath));
+
+    const keys1 = Object.keys(data1).sort();
+    // const keys2 = Object.keys(data2).sort();
+
+    const diff = keys1.map((key) => {
+        if(data1[key] === data2[key]) {
+            return {key: key, value: data1[key], status: 'equal'}
+        }
+        if(data1[key] !== data2[key]) {
+            return {key: key, value1: data1[key], value2: data2[key], status: 'unequal'}
+        }
+        if(data1.hasOwnProperty(key)) {
+            return {key: key, value: data1[key], status: 'deleted'}
+        }
+        if(data2.hasOwnProperty(key)) {
+            return {key: key, value: data2[key], status: 'added'}
+        }
+    })
+
+    return diff;
 };
 
 export default genDiff;
