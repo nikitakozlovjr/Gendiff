5c5faf7288ffbbc858ec20b23c2bb833ebd3c7d7
nikitakozlovjr <nikita3726898@gmail.com>
2023-11-20 23:35:21 +0300

    Добавили action

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,13 +1,16 @@
 #!/usr/bin/env node
 
 import { program } from 'commander';
-
+import genDiff from '../src/index.js';
 
 program
     .name('genDiff')
     .description('Compares two configuration files and shows a difference.')
     .version('1.0.0')
     .option('-f, --format <type>', 'output format')
-    .arguments('<filepath1> <filepath2>');
+    .arguments('<filepath1> <filepath2>')
+    .action((file1, file2) => {
+        console.log(genDiff(file1, file2));
+    });
 
 program.parse();
