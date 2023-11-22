5e4bd697cb662b82cd74de63ca33585c002b27fb
nikitakozlovjr <nikita3726898@gmail.com>
2023-11-22 16:18:06 +0300

    Добавили тесты на вложенные файлы

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -9,8 +9,14 @@ const __dirname = path.dirname(__filename);
 const getPath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 
 const correctFlatDiff = fs.readFileSync(getPath('expectFlatObj.txt'), 'utf-8');
+const correctNestedDiff = fs.readFileSync(getPath('expectNestedObj.txt'), 'utf-8')
 
-test('Reconciliation of flat JSON files', () => {
+test('Reconciliation of flat files', () => {
     expect(genDiff('../__fixtures__/file1.json', '../__fixtures__/file2.json')).toEqual(correctFlatDiff);
     expect(genDiff('../__fixtures__/file1.yml', '../__fixtures__/file2.yml').toEqual(correctFlatDiff))
+})
+
+test('Reconciliation of nested files', () => {
+    expect(genDiff('../__fixtures__/file1.json', '../__fixtures__/file2.json')).toEqual(correctNestedDiff);
+    expect(genDiff('../__fixtures__/file1.yml', '../__fixtures__/file2.yml').toEqual(correctNestedDiff))
 })
