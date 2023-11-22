080324cd37290b8b50511fef07bee7c5fc51fea4
nikitakozlovjr <nikita3726898@gmail.com>
2023-11-23 02:38:59 +0300

    Прописали форматирование диффа

--- src/index.js
+++ src/index.js
@@ -2,21 +2,21 @@ import parse from "./parser.js";
 import path from 'path';
 import getDiff from './getDiff.js';
 import { fileURLToPath } from 'url';
-import stylish from './formatter/stylish.js';
+import formatter from './formatter/index.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const buldpath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 
-const genDiff = (file1, file2) => {
+const genDiff = (file1, file2, format = 'stylish') => {
     const filepath1 = buldpath(file1);
     const filepath2 = buldpath(file2);
 
     const [data1, data2] = [filepath1, filepath2].map((filepath) => parse(filepath));
 
     const diff = getDiff(data1, data2);
-    return stylish(diff)
+    return formatter(format, diff)
 };
 
 export default genDiff;
