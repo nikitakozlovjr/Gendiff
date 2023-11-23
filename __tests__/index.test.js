5ad7d2a2b8869401be7ed6902b7ba968e4ca846c
nikitakozlovjr <nikita3726898@gmail.com>
2023-11-24 01:43:06 +0300

    Написали проверку на plain формат

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -9,14 +9,20 @@ const __dirname = path.dirname(__filename);
 const getPath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 
 const correctFlatDiff = fs.readFileSync(getPath('expectFlatObj.txt'), 'utf-8');
-const correctNestedDiff = fs.readFileSync(getPath('expectNestedObj.txt'), 'utf-8')
+const correctNestedDiff = fs.readFileSync(getPath('expectNestedObj.txt'), 'utf-8');
+const correctPlainFormat = fs.readFileSync(getPath('expectPlainFormat.txt'), 'utf-8');
 
 test('Reconciliation of flat files', () => {
-    expect(genDiff('../__fixtures__/file1.json', '../__fixtures__/file2.json')).toEqual(correctFlatDiff);
-    expect(genDiff('../__fixtures__/file1.yml', '../__fixtures__/file2.yml').toEqual(correctFlatDiff))
+    expect(genDiff('file1.json', 'file2.json')).toEqual(correctFlatDiff);
+    expect(genDiff('file1.yml', 'file2.yml')).toEqual(correctFlatDiff);
 })
 
 test('Reconciliation of nested files', () => {
-    expect(genDiff('../__fixtures__/file1.json', '../__fixtures__/file2.json')).toEqual(correctNestedDiff);
-    expect(genDiff('../__fixtures__/file1.yml', '../__fixtures__/file2.yml').toEqual(correctNestedDiff))
+    expect(genDiff('file3.json', 'file4.json')).toEqual(correctNestedDiff);
+    expect(genDiff('file3.yml', 'file4.yml')).toEqual(correctNestedDiff);
+})
+
+test('Checking the correctness of the plain formatter', () => {
+    expect(genDiff('file3.json', 'file4.json', 'plain')).toEqual(correctPlainFormat);
+    expect(genDiff('file3.yml', 'file4.yml', 'plain')).toEqual(correctPlainFormat);
 })
