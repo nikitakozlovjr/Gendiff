3ab0d36513479160c96565b5bbb2f260d9dd3e28
nikitakozlovjr <nikita3726898@gmail.com>
2023-11-24 00:43:06 +0300

    Исправили сортировку ключей

--- src/getDiff.js
+++ src/getDiff.js
@@ -1,10 +1,10 @@
 import _ from 'lodash';
 
 const getDiff = (data1, data2) => {
-    const keys1 = _.sortBy(Object.keys(data1));
-    const keys2 = _.sortBy(Object.keys(data2));
+    const keys1 = Object.keys(data1);
+    const keys2 = Object.keys(data2);
 
-    const keys = _.uniq(keys1.concat(keys2));
+    const keys = _.sortBy(_.uniq(keys1.concat(keys2)));
     
     const diff = keys.map((key) => {
         if(!Object.hasOwn(data1, key)) {
