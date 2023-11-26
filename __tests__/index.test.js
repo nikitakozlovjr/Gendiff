824f2dd446aaa8cbbdf71844e3ffcc23fc07dbd7
nikitakozlovjr <nikita3726898@gmail.com>
2023-11-26 13:48:05 +0300

    Добавили 2 блока тестов: сравнение плоских и вложенных файлов

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -14,25 +14,26 @@ const promises = reconciliationFile.map((filename) => fsp.readFile(getPath(filen
 
 // ----------- Переменные хранят правильные данные, использующиеся для проверки ------------
 const extentions = ['json', 'yml'];
-const formats = ['stylish', 'palin'];
+const formats = ['stylish', 'palin', 'json'];
 const [expectFlatObj, expectNestedObj, expectPlainFormat] = await Promise.all(promises);
 // -----------------------------------------------------------------------------------------
 
-test('Reconciliation of flat files', () => {
-    expect(genDiff('file1.json', 'file2.json')).toEqual(expectFlatObj);
-    expect(genDiff('file1.yml', 'file2.yml')).toEqual(expectFlatObj);
-})
+describe('Correct flat file comparison', () => {
+    test.each(extentions)('flat test %s', (extension) => {
+      const filepath1 = getPath(`file1.${extension}`);
+      const filepath2 = getPath(`file2.${extension}`);
+  
+      expect(genDiff(filepath1, filepath2)).toEqual(expectFlatObj);
+      expect(genDiff(filepath1, filepath2, 'stylish')).toEqual(expectFlatObj);
+    });
+  });
+  
+  describe('Correct nested file comparison', () => {
+    test.each(extentions)('nested test %s', (extension) => {
+      const filepath1 = getPath(`file3.${extension}`);
+      const filepath2 = getPath(`file4.${extension}`);
 
-test('Reconciliation of nested files', () => {
-    expect(genDiff('file3.json', 'file4.json')).toEqual(expectNestedObj);
-    expect(genDiff('file3.yml', 'file4.yml')).toEqual(expectNestedObj);
-})
-
-test('Checking the correctness of the plain formatter', () => {
-    expect(genDiff('file3.json', 'file4.json', 'plain')).toEqual(expectPlainFormat);
-    expect(genDiff('file3.yml', 'file4.yml', 'plain')).toEqual(expectPlainFormat);
-})
-
-test('Checking the correct operation of the parser', () => {
-    expect(() => genDiff('file1.false', 'file2.false', 'plain')).toThrow('Неподдерживаемый формат файла');
-})
+      expect(genDiff(filepath1, filepath2)).toEqual(expectNestedObj);
+      expect(genDiff(filepath1, filepath2, 'stylish')).toEqual(expectNestedObj);
+    })
+  });
